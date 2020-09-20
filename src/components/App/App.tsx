029ab000deeac6531f221e17b95b2748c24b4d91
marco varela <marco.varela12@gmail.com>
2020-09-20 22:35:47 +0100

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Recorder from '../Recorder/Recorder';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Recorder/>
+    </div>
+  );
+}
+
+export default App;
